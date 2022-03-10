f687da7c35e11795d992fc7b6148c9367c5c5405
omidvari80 <mlkmhby6@gmail.com>
2022-03-10 23:31:18 +0330

    add: js

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-
+const SECONDHAND = document.querySelector('.second-hand');
+const MINSHAND = document.querySelector('.min-hand');
+const HOURHAND = document.querySelector('.hour-hand');
       
+function setDate() {
+  const NOW = new Date();
+  const SECONDS = NOW.getSeconds();
+  const SECONDSDEGREES = ((SECONDS / 60) * 360) + 90;
+  SECONDHAND.style.transform = `rotate(${SECONDSDEGREES}deg)`;
+
+  const MINS = NOW.getMinutes();
+  const MINSDEGREES = ((MINS / 60) * 360) + ((SECONDS/60)*6) + 90;
+ MINSHAND.style.transform = `rotate(${MINSDEGREES}deg)`;
+
+ const HOUR= NOW.getHours();
+  const HOURDEGREES = ((HOUR / 12) * 360) + ((MINS/60)*30) + 90;
+  HOURHAND.style.transform = `rotate(${HOURDEGREES}deg)`;
+}
+
+setInterval(setDate, 1000);
+
+setDate();
